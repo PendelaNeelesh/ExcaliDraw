72ae34c9ea3d59fa8cdaefd2ab4a78b016e1705a
PendelaNeelesh <pendelaneelesh.191ec136@nitk.edu.in>
2021-07-22 16:44:31 +0000

    fixed-duplicatefilenames

--- excali-proj/public/js/index.js
+++ excali-proj/public/js/index.js
@@ -2,6 +2,7 @@
 import InitialData from "./initialData.js";
 var saveClicks =1;
 var stopClicks =0;
+var doesExist =0;
 var xyz;
 const App = () => {
   const excalidrawRef = React.useRef(null);
@@ -90,51 +91,60 @@ const App = () => {
                         console.log('data');
                     },
                     error: function (jqXHR, exception) {
-                      alert("File Name Already exists, Stop Saving and StartSaving Again");
+                      alert("File Name Already exists"); 
+                      saveClicks--;
+                      clearInterval(xyz);
+                      console.log("Cleared time")
+                      doesExist =1;
 
                     }
                 });
           
                   var ele = excalidrawRef.current.getSceneElements();
-    
-                  xyz = setInterval(async () => {
-                    console.log("Ready at");
-                    let t = new Date();
-                    console.log(
-                      t.getHours() + ":" + t.getMinutes() + ":" + t.getSeconds()
-                    );
-                    if (ele !== excalidrawRef.current.getSceneElements()) {
-                      console.log("not equal");
-                      const svg = await window.Excalidraw.exportToSvg({
-                        elements: excalidrawRef.current.getSceneElements(),
-                        appState: {
-                          ...InitialData.appState,
-                          width: 300,
-                          height: 100
-                        },
-                        embedScene: true
+                  if(doesExist==0){
+                    xyz = setInterval(async () => {
+                      console.log("Ready at");
+                      let t = new Date();
+                      console.log(
+                        t.getHours() + ":" + t.getMinutes() + ":" + t.getSeconds()
+                      );
+                      if (ele !== excalidrawRef.current.getSceneElements()) {
+                        console.log("not equal");
+                        const svg = await window.Excalidraw.exportToSvg({
+                          elements: excalidrawRef.current.getSceneElements(),
+                          appState: {
+                            ...InitialData.appState,
+                            width: 300,
+                            height: 100
+                          },
+                          embedScene: true
+                        });
+                        $.ajax({
+                          url: '/saveImage',
+                          type: "POST",
+                          data: {
+                              svgText: svg.outerHTML,
+                              Filename: fname,
+                              ver: version++
+                              
+                          },
+                          dataType: "application/json",
+                          success: function (data) {
+                              console.log('data');
+                          },
+                          error: function (jqXHR, exception) {
+                            console.log('abc');
+                              console.log(`Error ${jqXHR.status}`+ exception);
+                          }
                       });
-                      $.ajax({
-                        url: '/saveImage',
-                        type: "POST",
-                        data: {
-                            svgText: svg.outerHTML,
-                            Filename: fname,
-                            ver: version++
-                            
-                        },
-                        dataType: "application/json",
-                        success: function (data) {
-                            console.log('data');
-                        },
-                        error: function (jqXHR, exception) {
-                          console.log('abc');
-                            console.log(`Error ${jqXHR.status}`+ exception);
-                        }
-                    });
-                      ele = excalidrawRef.current.getSceneElements();
-                    }
-                  }, 10000);
+                        ele = excalidrawRef.current.getSceneElements();
+                      }
+                    }, 10000);
+                  }
+                  
+              }
+              else{
+                alert("Enter File name")
               }
               }
               else{
